9abb03ff32e6af55f70d026e2da2d708ca59fdbe
Alex Sanchez <alexanderclaysanchez@gmail.com>
2020-09-07 00:37:09 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -48,7 +48,15 @@ class Song
   
   # returns a hash in which the keys are the names of each artist; same as above?
   def self.artist_count
-    
+    artist_count = {}
+    @@artists.each do |artist|
+      if artist_count[artist]
+        artist_count[artist] += 1
+      else
+        artist_count[artist] = 1
+      end
+    end
+    artist_count
   end
   
 end
